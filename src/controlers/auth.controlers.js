c81139ce0224b7a9b18bd78411205a94b63497a6
Gopala <guptaraja727@gmail.com>
2022-01-13 23:42:52 +0530

    Update auth.controlers.js

--- src/controlers/auth.controlers.js
+++ src/controlers/auth.controlers.js
@@ -3,7 +3,9 @@ const User = require("../models/user.model")
 
 const register = async(req,res)=>{
     let user =  await User.findOne({email:req.body.email}).lean().exec()
+
     if(user) return res.status(400).send({message:"Email already register"})
+    
     user = await User.create(req.body)
    return res.send("Register")
 }
