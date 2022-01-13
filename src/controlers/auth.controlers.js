b701c75c3c9e0e86f72d9826890995a5d7217c6b
Gopala <guptaraja727@gmail.com>
2022-01-14 00:47:00 +0530

    c

--- src/controlers/auth.controlers.js
+++ src/controlers/auth.controlers.js
@@ -2,12 +2,16 @@
 const User = require("../models/user.model")
 
 const register = async(req,res)=>{
+   try{
     let user =  await User.findOne({email:req.body.email}).lean().exec()
 
     if(user) return res.status(400).send({message:"Email already register"})
-    
+
     user = await User.create(req.body)
-   return res.send("Register")
+   }catch(err){
+       return res.status(500).send({message:err.message})
+   }
+   return res.send(user)
 }
 
 const login = async(req,res)=>{
--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,11 +1,23 @@
 const mongoose = require("mongoose")
 
+const bcrypt = require("bcryptjs")
+
 const userSchema = new mongoose.Schema({
     email:{type:String,required:true,unique:true},
     password:{type:String,required:true}
 },{
    versionKey:false,
    timestamps:true 
+})
+
+userSchema.pre("save",(next)=>{
+    if(!this.isModified("password")) return next()
+
+    bcrypt.hash(this.password, 8, function(err, hash) {
+        if(err) return next(err)
+        user.password = hash
+        return next()
+    });
 })
 
 module.exports = mongoose.model("users",userSchema)
