a3a518e7835691d34061662e79020743122e6012
Gopala <guptaraja727@gmail.com>
2022-01-15 23:26:43 +0530

    Update authenticate.js

--- src/middlewere/authenticate.js
+++ src/middlewere/authenticate.js
@@ -1,31 +1,31 @@
 
-require("dotenv").config()
+// require("dotenv").config()
 
-const jwt = require("jsonwebtoken");
-const { verify } = require("jsonwebtoken");
+// const jwt = require("jsonwebtoken");
+// const { verify } = require("jsonwebtoken");
 
-const verifyToken = (tocken)=>{
-    return new Promise((resolve,reject)=>{
-        jwt.verify(tocken,process.env.JWT_SECRET_KEY, function(err,decode){
-            if(err) return reject(err)
-            return (decode)
-        })
-    })
-}
+// const verifyToken = (tocken)=>{
+//     return new Promise((resolve,reject)=>{
+//         jwt.verify(tocken,process.env.JWT_SECRET_KEY, function(err,decode){
+//             if(err) return reject(err)
+//             return (decode)
+//         })
+//     })
+// }
 
 module.exports = (req, res, next) => {
-  if (!req?.headers?.authorization)
-    return res.status(400).send({ message:"Bad request" });
+//   if (!req?.headers?.authorization)
+//     return res.status(400).send({ message:"Bad request" });
 
-    const bearerTocken = req.headers.authorization;
+//     const bearerTocken = req.headers.authorization;
 
-    if(!bearerTocken.startsWith("Bearer"))
-    return res.status(400).send({message:"Bad request"})
+//     if(!bearerTocken.startsWith("Bearer"))
+//     return res.status(400).send({message:"Bad request"})
 
-    const tocken = bearerTocken.split(" ")[1]
+//     const tocken = bearerTocken.split(" ")[1]
 
-    const user = await verifyToken(tocken)
+//     const user = await verifyToken(tocken)
 
- req.user = user
+//  req.user = user
   next();
 };
